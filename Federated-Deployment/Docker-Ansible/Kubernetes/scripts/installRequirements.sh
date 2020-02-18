58548f358214efeee8b05c0b9ede67ef901c7b13
LSmyrnaios <lsmyrnaios@gmail.com>
2020-02-18 21:18:28 +0200

    Kubernetes: update "installRequirements.sh" to install a newer version of Kubernetes and "hold" it there to not be updated automatically, since certain features work with specific major versions of Kubernetes.

--- Federated-Deployment/Docker-Ansible/Kubernetes/scripts/installRequirements.sh
+++ Federated-Deployment/Docker-Ansible/Kubernetes/scripts/installRequirements.sh
@@ -25,7 +25,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - \
 && echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list \
 && sudo apt-get update \
-&& sudo apt-get install -y kubeadm=1.15.9-00 kubectl=1.15.9-00 kubelet=1.15.9-00
+&& sudo apt-mark unhold kubeadm kubectl kubectl \
+&& sudo apt-get install -y kubeadm=1.15.10-00 kubectl=1.15.10-00 kubelet=1.15.10-00 \
+&& sudo apt-mark hold kubeadm kubectl kubectl
 
 # Install python in order for ansible to work and JQ for parsing the output of some commands.
 sudo apt-get install -y python jq
