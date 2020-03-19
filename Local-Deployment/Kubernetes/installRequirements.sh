46f040b0a081cae369f3cea3e5fe93c23b34de99
LSmyrnaios <lsmyrnaios@gmail.com>
2020-03-20 01:23:44 +0200

    Kubernetes' updates: - Re-organize "KubeFiles"; have dedicated sub-dirs for master's and workers' resources, rename the python-script. - Add some initial docker-configurations in "installRequirements.sh"-scripts.

--- Local-Deployment/Kubernetes/installRequirements.sh
+++ Local-Deployment/Kubernetes/installRequirements.sh
@@ -19,7 +19,26 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
 && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 && sudo apt-get update \
 && sudo apt-get install -y docker-ce docker-ce-cli containerd.io \
-&& sudo docker run hello-world
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+sudo systemctl daemon-reload \
+&& sudo systemctl restart docker
+
+sudo docker run hello-world
 
 # Create docker super-user
 groupadd docker \
